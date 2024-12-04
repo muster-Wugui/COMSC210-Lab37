1a7b65fe0f8fcfc7b8d05e368783db302b288e05
muster-Wugui <tintinpost28@gmail.com>
2024-12-04 11:29:58 -0800

    Step 1

--- COMSC210-Lab37/main.cpp
+++ COMSC210-Lab37/main.cpp
@@ -1,3 +1,5 @@
+// COMSC-210 | Lab 37 | Xiao Zhang
+
 //
 //  main.cpp
 //  COMSC210-Lab37
@@ -6,9 +8,43 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int sum_ascii(string st);
+
+int main() {
+    char a = 'A';
+    cout << a << endl;
+    cout << (int) a << endl;
+    int b = 66;
+    cout << b << endl;
+    cout << (char) b << endl;
+    
+    string test;
+    cout << "Enter a string: ";
+    cin >> test;
+    
+    cout << "The sum of ASCII values: " << sum_ascii(test) << endl; //Print out the sum
+    
+
     return 0;
 }
+
+int sum_ascii(string st){
+    int sum = 0;
+    
+    for (int i = 0; i < st.length(); i++) {
+           sum += (int)st[i]; // This is the step to cast each character to its ASCII value and add to sum
+       }
+    
+    return sum;
+}
+
+/*
+These targets are present in the dataset and can be used for testing:
+536B9DFC93AF
+1DA9D64D02A0
+666D109AA22E
+E1D2665B21EA
+*/
